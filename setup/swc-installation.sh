8db7fb794fd1f07b5efe201035a337a4dcab0a0b
davecash75 <d.cash@ucl.ac.uk>
2023-03-06 14:23:35 +0000

    Put a check that environment is activated

--- setup/swc-installation.sh
+++ setup/swc-installation.sh
@@ -6,11 +6,21 @@ then
 fi
 env_name="python_course"
 script_dir=`dirname ${BASH_SOURCE}`
+# Since we are running this within a script, we need to source 
+# this first, lest we want errors about conda init in this
 source ${CONDA_PREFIX}/etc/profile.d/conda.sh
 echo "Creating environment ${env_name}..."
 conda create -y -n ${env_name} python=3.9 pip
 echo "Activating environment ${env_name}..."
 conda activate ${env_name}
+# After an activate the CONDA_DEFAULT_ENV variable should change
+# This is a STOP point in case it doesn't so that you aren't 
+# messing with the base env
+if [ "${CONDA_DEFAULT_ENV}" != "${env_name}" ]
+then
+    echo "The activation failed for some reason, so I'm bailing out of the install" 
+    exit
+fi
 echo "Installing requirements..."
 pip3 install -r ${script_dir}/requirements.txt
 
